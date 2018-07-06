5de3c339762aee910d87c4ef5c28bfbbc623972c
demidkin <demidkin.spb@gmail.com>
2018-07-06 19:07:36 +0300

    transactions

--- src/actions/loadTransactions.action.js
+++ src/actions/loadTransactions.action.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export function loadTransactions (userData) {
     return dispatch => {
-        return axios.post('http://localhost:3000/api/getTransactions', userData);
+        axios.post('http://localhost:3000/api/getTransactions', userData)
     }
 }
